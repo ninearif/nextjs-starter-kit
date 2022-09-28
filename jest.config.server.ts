df64dbaecaef75a141a0675ca44f4761db958640
ninearif <ninearif@hotmail.com>
2022-09-28 16:47:49 +0700

    test: add jestSetup for server-side

--- jest.config.server.ts
+++ jest.config.server.ts
@@ -8,6 +8,7 @@ const customServerConfig = {
   // match test with .server.spec|test.ts(x)
   testMatch: ['**/?(*.)+(server).(spec|test).[jt]s'],
   testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.server.ts'],
 };
 
 export default createJestConfig(customServerConfig);
